dfed7c2c4fe2b4b4989165ec308e139c8aa395bd
Eshaan Garg <eshaangarg13@gmail.com>
2026-09-09 13:19:57 +0530

    revised potd

--- LeetCode/POTDs/09-09-2026.cpp
+++ LeetCode/POTDs/09-09-2026.cpp
@@ -3,13 +3,10 @@ public:
     long long countCommas(long long n) {
         long long ans = 0;
         long long cur = 1e3;
-        int iter = 1;
+        // int iter = 1;
         while(cur <= n){
-            long long next = cur*1e3;
-            next--;
-            ans += iter*(min(n,next)-cur+1);
+            ans += n-cur+1;
             cur = cur*1e3;
-            iter++;
         }
         return ans;
     }
